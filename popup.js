931af48e02fd739ca1ce0361219d79f7f08d623c
Mathias Decaille <mat.decaille@gmail.com>
2022-07-23 22:14:13 -0400

    initial commit: basic UI + no logic

---
+++ popup.js
@@ -0,0 +1,28 @@
+// popup.js
+
+/** Open Settings */
+
+function openSettings() {
+    console.log("not implemented yet...");
+}
+
+/** Switch click */
+function toogleMainSwitch(){
+    console.log("not implemented yet...");
+}
+
+/** Name click */
+function nameClicked(){
+    console.log("not implemented yet...");
+}
+
+/** Linking DOM with functions */
+document.addEventListener('DOMContentLoaded', function() {
+    // settings button
+    var settingsButton = document.getElementById('settings-button');
+    settingsButton.addEventListener('click', function() {
+        openSettings();
+    });
+
+    // main switch
+});
